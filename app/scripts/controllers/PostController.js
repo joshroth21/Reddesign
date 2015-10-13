b6d22688bc75387b3b86fceab6f9cb66a4e6eb7e
Josh Roth <josh@joshroth.me>
2015-10-13 09:59:34 -0700

    Updated post view

--- app/scripts/controllers/PostController.js
+++ app/scripts/controllers/PostController.js
@@ -1,10 +1,13 @@
 'use strict';
 
-angular.module('reddesign00App').controller('PostController',['$scope','$routeParams','$http',function($scope, $routeParams, $http) {
+var app = angular.module('reddesign00App');
+
+app.controller('PostController',['$scope','$routeParams','$http','$sce',function($scope, $routeParams, $http, $sce) {
 	$scope.postName = $routeParams.postName;
 	$http.get('https://www.reddit.com/by_id/' + $scope.postName + '.json')
 		.success(function(data){
 			$scope.post = data.data.children[0].data;
+			$scope.postMediaEmbed = $sce.trustAsHtml($scope.post.secure_media_embed.content);
 		})
 		.error(function(data){
 			return data;
