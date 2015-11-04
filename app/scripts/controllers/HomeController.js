60598545e565b94d6bb63555bb71abb09d35b0df
Josh Roth <josh@cleanenergyexperts.com>
2015-11-04 09:44:56 -0800

    Redesigned homepage

--- app/scripts/controllers/HomeController.js
+++ app/scripts/controllers/HomeController.js
@@ -2,4 +2,12 @@
 
 angular.module('Reddesign').controller('HomeController', function($scope, topPosts) {
 	$scope.topPosts = new topPosts();
+	$scope.showFullPreview = function() {
+		console.log('show');
+		return true;
+	};
+	$scope.hideFullPreview = function() {
+		console.log('hide');
+		return false;
+	};
 });
